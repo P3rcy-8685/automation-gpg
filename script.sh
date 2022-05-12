994070e545cdf797cadddceac1b3c9aa10c866bd
Perseus-Jackson477 <vanshuppal2002@gmail.com>
2022-05-12 11:29:46 +0530

    first commit

---
+++ script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash\
+keyVal=$(gpg --list-secret-keys --keyid-format=long|awk '/uid/{if (length($3)>0) print $3}')
+echo "enter 0 if you want to generate new gpg key. Otherwise input 1,2,3 respectively for particular user ID"
+a=1
+while IFS= read -r line;
+	do echo  "${a} '${line}'";
+	export a=$((a+1))
+done<<<"$keyVal"
+no_of_keys=a
+read val
+keyVal=$(gpg --list-secret-keys --keyid-format=long|awk '/sec/{if (length($2)>0) print $2}')
+a=1
+while IFS= read -r line;
+do
+	if [[ "$val" == "$a" ]]; then
+		echo "Enter the following in your git gpg portion"
+		out=${line##*/}
+		echo out
+		gpg --armor --export $out
+		git config --global user.signingkey $out
+		git config --global commit.gpgsign true
+		break
+	elif [[ "$val" > "$no_of_keys" ]];then
+		echo "Please enter a valid key"
+		break
+	fi
+	export a=$((a+1))
+done<<<"$keyVal"
+if [[ "$val" == "0" ]]; then
+gpg --default-new-key-algo rsa4096 --gen-key
+keyVal=$(gpg --list-secret-keys --keyid-format=long|awk '/sec/{if (length($2)>0) print $2}')
+a=1
+export no_of_keys=$((no_of_keys+1))
+while IFS= read -r line;
+do
+if [[ "$no_of_keys" == "$a" ]]; then
+	echo "paste the following in your gpg region"
+        out=${line##*/}
+        echo out
+        gpg --armor --export $out
+        git config --global user.signingkey $out
+        git config --global commit.gpgsign true
+        break
+fi
+export a=$((a+1))
+done<<<"$keyVal"
+fi
+
+
